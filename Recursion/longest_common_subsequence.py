3445915b771ea65516494c69eea2e71520413283
Tarun24verma <Tarunverma20070724@gmail.com>
2026-07-02 10:02:21 +0530

    Refactor Tree structure and implement longest common subsequence function; update node connections and adjust print logic

--- Recursion/longest_common_subsequence.py
+++ Recursion/longest_common_subsequence.py
@@ -1,6 +1,12 @@
-# s1="asddfg"
-# s2="dd"
-# a=set(s1)
-# b=set(s2)
-# c=a.intersection(b)
-# print(len(c))
+s1 = "abcdefg"
+s2 = "acdfg"
+m = len(s1)
+n = len(s2)
+def lcs1(s1, s2, m, n):
+    if m == 0 or n == 0:
+        return 0
+    if s1[m - 1] == s2[n - 1]:
+        return 1 + lcs1(s1, s2, m - 1, n - 1)
+    else:
+        return max(lcs1(s1, s2, m, n - 1), lcs1(s1, s2, m - 1, n))
+print(lcs1(s1, s2, m, n))
--- Tree/Tree.py
+++ Tree/Tree.py
@@ -4,30 +4,33 @@ class Tree:
         self.right=None
         self.left=None
 root=Tree(20)
-root.left=Tree(10)
-root.right=Tree(25)
-root.left.left=Tree(9)
-root.left.left.left=Tree(8)
-root.left.right=Tree(15)
-root.right.left=Tree(22)
-root.right.right=Tree(30)
+root.left=Tree(8)
+root.right=Tree(22)
+root.left.left=Tree(5)
+root.left.right=Tree(3)
+root.left.right.left=Tree(10)
+root.left.right.right=Tree(14)
+root.right.right=Tree(25)
+root.right.left=Tree(4)
+root.right.right.left=Tree(28)
 def Tprint(root):
     if not root:
         return
-    Tprint(root.right)
-    print(root.data)
     Tprint(root.left)
+    if root.left==None and root.right==None:
+        print(root.data)
+    Tprint(root.right)
 Tprint(root)
-s=21
-found=0
-def Search(root,s):
-    if root:
-        if root.data==None:
-            print(False)
-        if root.data == s:
-            print(True)
-        elif s < root.data:
-            return Search(root.left,s)
-        else:
-            return Search(root.right,s)
-Search(root,s)
+# s=4
+# found=0
+# def Search(root,s):
+#     if root:
+#         if root.data==None:
+#             print(False)
+#         if root.data == s:
+#             print(True)
+#         elif s < root.data:
+#             return Search(root.left,s)
+#         else:
+#             return Search(root.right,s)
+# Search(root,s)
